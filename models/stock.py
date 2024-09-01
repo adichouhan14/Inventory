713eac96f8aa44cb65fb3ab4e1d7516839ff7ba9
Aditya <adichouhan14@gmail.com>
2024-09-01 18:22:29 +0530

    Stock insert update delete logic update - need testing & bug fixing

--- models/stock.py
+++ models/stock.py
@@ -5,7 +5,7 @@ class Stock(db.Model):
     __tablename__ = 'stock'
     
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False, unique=True)
     product_quantity = db.Column(db.Integer, nullable=False)
     last_update_date = db.Column(db.DateTime, default=datetime.utcnow)
     
